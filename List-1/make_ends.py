98585dc9ec74457d0cb60a0a8c3a21b5ff5efd4e
Michał <62473553+michalpucci@users.noreply.github.com>
2020-04-21 12:01:36 +0200

    Add files via upload

---
+++ List-1/make_ends.py
@@ -0,0 +1,11 @@
+# Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+#
+#
+# make_ends([1, 2, 3]) → [1, 3]
+# make_ends([1, 2, 3, 4]) → [1, 4]
+# make_ends([7, 4, 6, 2]) → [7, 2]
+
+def make_ends(nums):
+  x = nums[0]
+  y = nums[len(nums)-1]
+  return [x,y]
